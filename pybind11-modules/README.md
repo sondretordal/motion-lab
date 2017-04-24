22e4a18d27885ebe4a4735a754b3e00e149135e8
Sondre Sanden Tørdal <sondre.tordal@uia.no>
2017-04-24 11:02:24 +0200

    Started working on pybind11 moduele for Udp comm

---
+++ pybind11-modules/README.md
@@ -0,0 +1,9 @@
+Cmake setup:
+cd build
+cmake .. -G "Visual Studio 14 2015 Win64"
+cd ..
+
+Cmake build:
+cd build
+cmake --build . --config Release
+cd ..
---
+++ pybind11-modules/main.cpp
@@ -0,0 +1,26 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+#include <pybind11/stl.h>
+
+#include "udpserver.hpp"
+
+
+
+
+/*
+#include <iostream>
+// Test program to use udp server class
+int main()
+{
+  UdpServer udp;
+
+  udp.start();
+  for(int i = 0; i < 900; i++)
+    std::cout << "i = " << i << std::endl;
+
+  sleep(5);
+  udp.stop();
+
+  return 0;
+}
+*/
