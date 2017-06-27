64c8d398dd099bd9ccfb0898d2ceb257da3a575b
jantolsen <janthomasolsen@gmail.com>
2017-06-27 08:58:22 +0200

    Updated structure and variable names, added plot functionality to EM8000 and COMAU plot tabs

--- hmi/src/hmi_dev.py
+++ hmi/src/hmi_dev.py
@@ -485,3 +485,13 @@ class GUI(QMainWindow, gui_main):
             event.accept()
         else:
             event.ignore()
+
+# Main function
+def main():
+    app = QApplication(sys.argv)
+    window = GUI()
+    sys.exit(app.exec_())
+
+#Application startup
+if __name__ == '__main__':
+    main()
