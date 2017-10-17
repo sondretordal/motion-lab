c492ea8578f6d982a52234b2db6aa9e95ad0aa62
sondre1988 <Sondre>
2017-10-17 11:32:36 +0200

    Befor major upgrade of UDP interface

--- docs/stewart-platforms.rst
+++ docs/stewart-platforms.rst
@@ -45,6 +45,8 @@ The Euler angle sequence is defined by conventional rotation matrices. The expli
 
 .. math::
     \bm{R}^n_b(\bm{\Theta}_{nb}) = \bm{R}_x(\phi)\bm{R}_y(\theta)\bm{R}_z(\psi)
+    \hspace{5mm} \text{or} \hspace{5mm}
+    \bm{R}_z(\psi)\bm{R}_y(\theta)\bm{R}_x(\phi)
 
 The accompanying transformation between time derivatives of the Euler angles :math:`\dot{\bm{\Theta}}_{nb}` and the local body-fixed rotational velocities :math:`\bm{\omega}^b_{b/n}` is given by the following two equations:
 
@@ -65,6 +67,24 @@ The accompanying transformation between time derivatives of the Euler angles :ma
     \begin{bmatrix}
     0 \\ 0 \\ \dot{\psi}
     \end{bmatrix} \\
+
+.. math::
+    \underbrace{
+    \left[ \begin {array}{ccc} \cos \left( \psi \right) \cos \left( 
+    \theta \right) &-\sin \left( \psi \right) \cos \left( \theta \right) &
+    \sin \left( \theta \right) \\ \noalign{\medskip}\cos \left( \psi
+    \right) \sin \left( \theta \right) \sin \left( \phi \right) +\sin
+    \left( \psi \right) \cos \left( \phi \right) &-\sin \left( \psi
+    \right) \sin \left( \theta \right) \sin \left( \phi \right) +\cos
+    \left( \psi \right) \cos \left( \phi \right) &-\cos \left( \theta
+    \right) \sin \left( \phi \right) \\ \noalign{\medskip}-\cos \left( 
+    \psi \right) \sin \left( \theta \right) \cos \left( \phi \right) +\sin
+    \left( \psi \right) \sin \left( \phi \right) &\sin \left( \psi
+    \right) \sin \left( \theta \right) \cos \left( \phi \right) +\cos
+    \left( \psi \right) \sin \left( \phi \right) &\cos \left( \theta
+    \right) \cos \left( \phi \right) \end {array} \right]
+    }_{\bm{R}_{xyz}}
+
 
 
 The feedback signals accessible as described in the :ref:`remote-io-interface`.
--- pybind11-modules/test.py
+++ pybind11-modules/test.py
@@ -4,47 +4,129 @@ import matplotlib.pyplot as plt
 from build import motionlab as ml
 import json
 
+def S(a):
+    ax = a[0, 0]
+    ay = a[1, 0]
+    az = a[2, 0]
+    
+    ret = np.matrix([[  0, -az,  ay],
+                     [ az,   0, -ax],
+                     [-ay,  ax,   0]])
+    return ret
 
+def Rx(x):
+    ret = np.matrix([[ 1,         0,         0],
+                     [ 0, np.cos(x),-np.sin(x)],
+                     [ 0, np.sin(x), np.cos(x)]])
+    return ret
 
-s = ml.RemoteInterface(50060)
+def Ry(x):
+    ret = np.matrix([[ np.cos(x), 0, np.sin(x)],
+                     [         0, 1,         0],
+                     [-np.sin(x), 0, np.cos(x)]])
+    return ret
 
-s.start()
+def Rz(x):
+    ret = np.matrix([[ np.cos(x),-np.sin(x), 0],
+                     [ np.sin(x), np.cos(x), 0],
+                     [         0,         0, 1]])
+    return ret
 
+def Rq(q):
+    Q = q/np.linalg.norm(q)
+    
+    q0 = Q[0, 0]
+    qX = Q[1, 0]
+    qY = Q[2, 0]
+    qZ = Q[3, 0]
+    
+    R = np.matrix(np.zeros([3, 3]))
+    
+    R[0,0] = 1 - 2*qY**2 - 2*qZ**2
+    R[1,0] = 2*(qX*qY  + qZ*q0)
+    R[2,0] = 2*(qX*qZ - qY*q0)
+    
+    R[0,1] = 2*(qX*qY - qZ*q0)
+    R[1,1] = 1 - 2*qX**2 - 2*qZ**2
+    R[2,1] = 2*(qY*qZ + qX*q0)
+    
+    R[0,2] = 2*(qX*qZ + qY*q0)
+    R[1,2] = 2*(qY*qZ - qX*q0)
+    R[2,2] = 1 - 2*qX**2 - 2*qY**2
+    
+    return R
 
-s.async_log(1)
-time.sleep(20)
+def Rxyz(phi):
+    rx = phi[0]
+    ry = phi[1]
+    rz = phi[2]
+    
+    R = Rx(rx)*Ry(ry)*Rz(rz)
 
-s.save_log('data.json')
+    return R
 
+def Txyz(phi):
+    rx = phi[0]
+    ry = phi[1]
+    rz = phi[2]
 
+    cy = np.cos(ry)
+    sy = np.sin(ry)
 
-s.close()
+    cz = np.cos(rz)
+    sz = np.sin(rz)
 
+    T = np.matrix([[     cz,   -sz,  0],
+                   [  cy*sz, cy*cz,  0],
+                   [ -sy*cz, sy*sz, cy]])
+
+    T = 1.0/cy*T
+
+    return T
+
+# s = ml.RemoteInterface(50060)
+# s.start()
+# s.async_log(1)
+# time.sleep(40)
+# s.save_log('data.json')
+# s.close()
 
 with open('data.json') as data_file:    
     data = json.load(data_file)
 
-
 t = data['feedback']['t']
 
-t = np.array(t)
+t = np.matrix(t)
 t = t - t[0]
 
-z = np.array(data['feedback']['em8000']['heave'])
-z_t = np.array(data['feedback']['em8000']['heave_t'])
-z_tt = np.array(data['feedback']['em8000']['heave_tt'])
 
-plt.figure('position')
-plt.plot(t,z)
+phi = np.matrix(np.zeros([3, len(t)]))
+w =  np.matrix(np.zeros([3, len(t)]))
+phi_t =  np.matrix(np.zeros([3, len(t)]))
+m_phi_t =  np.matrix(np.zeros([3, len(t)]))
 
-plt.figure('velocity')
-plt.plot(t,z_t)
+phi[0,:] = np.matrix(data['feedback']['em8000']['pitch'])
+phi[1,:] = np.matrix(data['feedback']['em8000']['roll'])
+phi[2,:] = np.matrix(data['feedback']['em8000']['yaw'])
 
-plt.figure('acceleration')
-plt.plot(t,z_tt)
+w[0,:] = np.matrix(data['feedback']['em8000']['wx'])
+w[1,:] = np.matrix(data['feedback']['em8000']['wy'])
+w[2,:] = np.matrix(data['feedback']['em8000']['wz'])
+
+for i in range(0, len(t)):
+    m_phi_t[:,i] = Txyz(phi[:,i])*w[:,i]
+
+
+for i in range(1, len(t)):
+    dt = t[i]-t[i-1]
+    phi_t[:,i] = (phi[:,i] - phi[:,i-1])/dt
+
+    
+plt.figure()
+plt.plot(t, m_phi_t[0,:].T, 'r')
+plt.plot(t, phi_t[0,:].T, 'b')
+
 plt.show()
-
-
 
 
 
