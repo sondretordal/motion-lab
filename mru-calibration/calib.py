b2e6331c71ea75293a36f4b77e6e5b6999e2afba
sondre1988 <Sondre>
2017-10-25 10:58:59 +0200

    Updated the calibration feature with interpolation option. Added notes fromreMarkable

--- mru-calibration/calib.py
+++ mru-calibration/calib.py
@@ -1,48 +1,49 @@
 import numpy as np
 from scipy import optimize
+from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 
 def S(a):
-    ax = a[0, 0]
-    ay = a[1, 0]
-    az = a[2, 0]
+    ax = a[0]
+    ay = a[1]
+    az = a[2]
     
-    ret = np.matrix([[  0, -az,  ay],
-                     [ az,   0, -ax],
-                     [-ay,  ax,   0]])
+    ret = np.array([[  0, -az,  ay],
+                    [ az,   0, -ax],
+                    [-ay,  ax,   0]])
     return ret
 
 def Rx(x):
-    ret = np.matrix([[ 1,         0,         0],
-                     [ 0, np.cos(x),-np.sin(x)],
-                     [ 0, np.sin(x), np.cos(x)]])
+    ret = np.array([[ 1,         0,         0],
+                    [ 0, np.cos(x),-np.sin(x)],
+                    [ 0, np.sin(x), np.cos(x)]])
     return ret
 
 def Ry(x):
-    ret = np.matrix([[ np.cos(x), 0, np.sin(x)],
-                     [         0, 1,         0],
-                     [-np.sin(x), 0, np.cos(x)]])
+    ret = np.array([[ np.cos(x), 0, np.sin(x)],
+                    [         0, 1,         0],
+                    [-np.sin(x), 0, np.cos(x)]])
     return ret
 
 def Rz(x):
-    ret = np.matrix([[ np.cos(x),-np.sin(x), 0],
-                     [ np.sin(x), np.cos(x), 0],
-                     [         0,         0, 1]])
+    ret = np.array([[ np.cos(x),-np.sin(x), 0],
+                    [ np.sin(x), np.cos(x), 0],
+                    [         0,         0, 1]])
     return ret
 
 def Rxyz(phi):
-    rx = phi[0, 0]
-    ry = phi[1, 0]
-    rz = phi[2, 0]
+    rx = phi[0]
+    ry = phi[1]
+    rz = phi[2]
     
-    R = Rx(rx)*Ry(ry)*Rz(rz)
+    ret = Rx(rx).dot(Ry(ry)).dot(Rz(rz))
 
-    return R
+    return ret
 
 def Txyz(phi):
-    rx = phi[0, 0]
-    ry = phi[1, 0]
-    rz = phi[2, 0]
+    rx = phi[0]
+    ry = phi[1]
+    rz = phi[2]
 
     cy = np.cos(ry)
     sy = np.sin(ry)
@@ -50,109 +51,129 @@ def Txyz(phi):
     cz = np.cos(rz)
     sz = np.sin(rz)
 
-    T = np.matrix([[     cz,   -sz,  0],
-                   [  cy*sz, cy*cz,  0],
-                   [ -sy*cz, sy*sz, cy]])
+    ret = np.array([[     cz,   -sz,  0],
+                    [  cy*sz, cy*cz,  0],
+                    [ -sy*cz, sy*sz, cy]])
 
-    T = 1.0/cy*T
+    ret = 1.0/cy*ret
 
-    return T
+    return ret
 
 def rot2euler(R):
     pitch = np.arcsin(R[0,2])
     roll = np.arcsin(-R[1,2]/np.cos(pitch))
     yaw = np.arcsin(-R[0,1]/np.cos(pitch))
 
-    return np.matrix([roll, pitch,yaw]).T
+    return np.array([roll, pitch,yaw])
 
-def solve_mru_pose(x0, t, eta, eta_t, w_bb, v_mm, w_mm, plot=False):
-    # Prepare data for pose optimization
-    
-    Rnb = []
-    for i in range(eta.shape[1]):
-        Rnb.append(Rxyz(eta[3:6,i]))
+def solve_mru_pose(x0, dt, t, body, mru, plot=False):
+    # Interpolation data
+    time = np.arange(t[0], t[-1], dt)
 
+    phi = np.zeros([3, len(time)])
+    phi[0,:] = interp1d(t, body['phi'])(time)
+    phi[1,:] = interp1d(t, body['theta'])(time)
+    phi[2,:] = interp1d(t, body['psi'])(time)
+
+    phi_t = np.zeros([3, len(time)])
+    phi_t[0,:] = interp1d(t, body['phi_t'])(time)
+    phi_t[1,:] = interp1d(t, body['theta_t'])(time)
+    phi_t[2,:] = interp1d(t, body['psi_t'])(time)
+
+    v_mm = np.zeros([3, len(time)])
+    v_mm[0,:] = interp1d(t, mru['x_t'])(time)
+    v_mm[1,:] = interp1d(t, mru['y_t'])(time)
+    v_mm[2,:] = interp1d(t, mru['z_t'])(time)
+
+    w_mm = np.zeros([3, len(time)])
+    w_mm[0,:] = interp1d(t, mru['wx'])(time)
+    w_mm[1,:] = interp1d(t, mru['wy'])(time)
+    w_mm[2,:] = interp1d(t, mru['wz'])(time)
+
+    w_bb = np.zeros([3, len(time)])
+    for i in range(0, len(time)):
+        w_bb[:,i] = Txyz(phi[:,i]).T.dot(phi_t[:,i]).ravel()
+
+    # Optimiziation objective functions
     def orientation(x, w_bb, w_mm):
-        phi = np.matrix([x[0], x[1], x[2]]).T
-
-        # Calculate error
-        E = 0
-        for i in range(0, w_mm.shape[1]):
-            e = w_bb[:,i] - Rxyz(phi)*w_mm[:,i]
-            E = E + e.T*e
-
-        print('E = ', E[0,0])
-        return E[0,0]
-
-    def position(x, R, w_bb, v_mm):
-        r = np.matrix([x[0], x[1], x[2]]).T
+        phi = np.array([x[0], x[1], x[2]])
 
         # Calculate squared sum error
         E = 0
-        for i in range(0, v_mm.shape[1]):
-            e = R*v_mm[:,i] - S(w_bb[:,i])*r
-            E = E + e.T*e
+        for i in range(0, len(time)):
+            e = w_bb[:,i] - Rxyz(phi).dot(w_mm[:,i])
+            E = E + e.T.dot(e)
 
-        print('E = ', E[0,0])
-        return E[0,0]
+        print('E = ', E)
+        return E
+
+    def position(x, R, w_bb, v_mm):
+        r = np.array([x[0], x[1], x[2]])
+
+        # Calculate squared sum error
+        E = 0
+        for i in range(0, len(time)):
+            e = R.dot(v_mm[:,i]) - S(w_bb[:,i]).dot(r)
+
+            E = E + e.T.dot(e)
+
+        print('E = ', E)
+        return E
     
     # Optimize unknown parameters
     f = lambda x: orientation(x, w_bb, w_mm)
     res1 = optimize.minimize(f, x0[0:3], tol=0.01)
-    R = Rxyz(np.matrix(res1.x).T)
+    R = Rxyz(res1.x)
     print('*** Orientation Completed ***')
 
     f = lambda x: position(x, R, w_bb, v_mm)
     res2 = optimize.minimize(f, x0[3:6], tol=0.01)
-    r = np.matrix(res2.x).T
+    r = res2.x
     print('*** Position Completed ***')
 
     if plot:
-        v = np.matrix(np.zeros(v_mm.shape))
-        w = np.matrix(np.zeros(v_mm.shape))
-        _eta_t = np.matrix(np.zeros(eta.shape))
-        _w_bb = np.matrix(np.zeros(w_bb.shape))
+        _w_mm = np.zeros(w_bb.shape)
+        _v_mm = np.zeros(w_bb.shape)
 
-        for i in range(v.shape[1]):
-            v[:,i] = (Rnb[i]*R).T*(eta_t[0:3,i] + Rnb[i]*S(w_bb[:,i])*r)
-            w[:,i] = R.T*w_bb[:,i]
-
-            _eta_t[0:3,i] = Rnb[i]*R*v_mm[:,i] - Rnb[i]*S(w_bb[:,i])*r
-            _w_bb[:,i] = R*w_mm[:,i]
+        for i in range(len(time)):
+            _w_mm[:,i] = R.T.dot(w_bb[:,i])
+            _v_mm[:,i] = R.T.dot(S(w_bb[:,i])).dot(r)
 
         plt.close('all')
         plt.figure()
 
         plt.subplot(321)
-        plt.plot(t, eta_t[0,:].T, 'r', label='stewart')
-        plt.plot(t, _eta_t[0,:].T, 'k--', label='mru')
+        plt.plot(time, v_mm[0,:].T, 'r', label='mru')
+        plt.plot(time, _v_mm[0,:].T, 'k--', label='transformed')
         plt.ylabel('x-velocity')
         plt.legend()
+
         plt.subplot(323)
-        plt.plot(t, eta_t[1,:].T, 'g', label='stewart')
-        plt.plot(t, _eta_t[1,:].T, 'k--', label='mru')
+        plt.plot(time, v_mm[1,:].T, 'r', label='mru')
+        plt.plot(time, _v_mm[1,:].T, 'k--', label='transformed')
         plt.ylabel('y-velocity')
         plt.legend()
+
         plt.subplot(325)
-        plt.plot(t, eta_t[2,:].T, 'b', label='stewart')
-        plt.plot(t, _eta_t[2,:].T, 'k--', label='mru')
+        plt.plot(time, v_mm[2,:].T, 'r', label='mru')
+        plt.plot(time, _v_mm[2,:].T, 'k--', label='transformed')
         plt.ylabel('z-velocity')
         plt.xlabel('Time - [s]')
         plt.legend()
         
         plt.subplot(322)
-        plt.plot(t, w_bb[0,:].T, 'r', label='stewart')
-        plt.plot(t, _w_bb[0,:].T, 'k--', label='mru')
+        plt.plot(time, w_mm[0,:].T, 'r', label='mru')
+        plt.plot(time, _w_mm[0,:].T, 'k--', label='transformed')
         plt.ylabel('rx-velocity')
         plt.legend()
         plt.subplot(324)
-        plt.plot(t, w_bb[1,:].T, 'g', label='stewart')
-        plt.plot(t, _w_bb[1,:].T, 'k--', label='mru')
+        plt.plot(time, w_mm[1,:].T, 'g', label='mru')
+        plt.plot(time, _w_mm[1,:].T, 'k--', label='transformed')
         plt.ylabel('ry-velocity')
         plt.legend()
         plt.subplot(326)
-        plt.plot(t, w_bb[2,:].T, 'b', label='stewart')
-        plt.plot(t, _w_bb[2,:].T, 'k--', label='mru')
+        plt.plot(time, w_mm[2,:].T, 'b', label='mru')
+        plt.plot(time, _w_mm[2,:].T, 'k--', label='transformed')
         plt.ylabel('rz-velocity')
         plt.xlabel('Time - [s]')
         plt.legend()
