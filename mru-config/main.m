3a36431f902c058aa3a21abaaa807841c4541638
Sondre <Sondre>
2017-12-13 15:38:54 +0100

    Added matlab code for autogeneration of variavlist and data structure

---
+++ mru-config/main.m
@@ -0,0 +1,32 @@
+close all
+clear all
+clc
+
+% Load csv data into table format
+data = readtable('variablelist.csv');
+
+fileRxMru = fopen('RxMru.txt', 'w');
+fileVarString = fopen('VarString.txt', 'w');
+
+fprintf(fileRxMru, 'mruOK : BYTE; // Mru Status, 113=OK\n');
+fprintf(fileRxMru, 'dataLength : BYTE; // Data length\n');
+fprintf(fileRxMru, 'token : BYTE; // MRU token\n');
+
+N = length(data.Include);
+for i = 1:N
+    if data.Include(i)
+        
+        fprintf(fileRxMru, '%s : %s; // No. %s - %s - %s\n',...
+            data.VarName{i},...
+            data.DataType{i},...
+            num2str(data.VarNumber(i)),...
+            data.Unit{i},...
+            data.Description{i});
+        
+        fprintf(fileVarString, '%s ', num2str(data.VarNumber(i)));
+    end
+end
+
+
+fclose(fileRxMru);
+fclose(fileVarString);
